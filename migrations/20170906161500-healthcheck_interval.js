a652ce10e51292205f94f3df9931719d95785fbe
Jason Tolsma <jason.tolsma@turner.com>
2017-09-06 13:28:52 -0400

    Add healthcheck_interval column to database

---
+++ migrations/20170906161500-healthcheck_interval.js
@@ -0,0 +1,53 @@
+module.exports = {
+    up: function (queryInterface, DataTypes) {
+        let changes = [];
+
+        // healthcheck_interval
+        changes.push(
+            queryInterface.addColumn(
+                'Ports',
+                'healthcheck_interval',
+                {
+                    type: DataTypes.INTEGER,
+                    field: "healthcheck_interval",
+                    validate: {
+                        min: 1,
+                        max: 3600
+                    },
+                    defaultValue: 10,
+                    get() {
+                        return this.getDataValue('healthcheck_interval') || this.getDataValue('value');
+                    }
+                }
+            )
+        );
+
+        return Promise.all(changes);
+    },
+
+    down: function (queryInterface, DataTypes) {
+        let changes = [];
+
+        // healthcheck_interval
+        changes.push(
+            queryInterface.addColumn(
+                'Ports',
+                'healthcheck_interval',
+                {
+                    type: DataTypes.INTEGER,
+                    field: "healthcheck_interval",
+                    validate: {
+                        min: 1,
+                        max: 3600
+                    },
+                    defaultValue: 10,
+                    get() {
+                        return this.getDataValue('healthcheck_interval') || this.getDataValue('value');
+                    }
+                }
+            )
+        );
+
+        return Promise.all(changes);
+    }
+};
