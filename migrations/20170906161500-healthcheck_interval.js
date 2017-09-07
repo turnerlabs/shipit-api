5c311cc2c4fc3731c6e2fdbb145bd393eec296fd
Jason Tolsma <jason.tolsma@turner.com>
2017-09-07 13:02:04 -0400

    Do nothing for down

--- migrations/20170906161500-healthcheck_interval.js
+++ migrations/20170906161500-healthcheck_interval.js
@@ -32,26 +32,6 @@ module.exports = {
     down: function (queryInterface, DataTypes) {
         let changes = [];
 
-        // healthcheck_interval
-        changes.push(
-            queryInterface.addColumn(
-                'Ports',
-                'healthcheck_interval',
-                {
-                    type: DataTypes.INTEGER,
-                    field: "healthcheck_interval",
-                    validate: {
-                        min: 1,
-                        max: 3600
-                    },
-                    defaultValue: 10,
-                    get() {
-                        return this.getDataValue('healthcheck_interval') || this.getDataValue('value');
-                    }
-                }
-            )
-        );
-
         return Promise.all(changes);
     }
 };
